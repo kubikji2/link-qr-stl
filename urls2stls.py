088faab3c5f7961e54ba13d30ddb5b867e6cecf6
Jiri Kvant Kubik <jiri.kub@gmail.com>
2022-10-26 23:05:18 +0200

    [urls-to-stls] code refactoring.

--- urls2stls.py
+++ urls2stls.py
@@ -1,12 +1,10 @@
-import link2qr
+import url2qr
 import os
 
-FN = "urls/urls.txt"
-
 # load urls
-def load_urls():
+def load_urls(urls_file):
     short_urls = []
-    with open(FN,'r') as f:
+    with open(urls_file,'r') as f:
         for line in f.readlines():
             entries = line.strip().split("/")
             entries.pop(-2)
@@ -15,11 +13,13 @@ def load_urls():
     return short_urls
 
 def process_url(url):
-    #print(url)
     fn_base = url.split("/")[-2]
+    fn_path = "./qrs/qr-{}.scad-qr".format(fn_base)
+    var_name = "qr_arr"
     print("Processing file: {}".format(fn_base))
-    link2qr.save_to_file(link2qr.arr2openscad(link2qr.url2arr(url)), "qr_arr", "./qrs/qr-{}.scad-qr".format(fn_base))
-    os.system("cp qrs/qr-{}.scad-qr qrs/qr.scad-qr".format(fn_base))
+    url2qr.process_url_to_file(url, var_name, fn_path)
+
+    os.system("cp {} qrs/qr.scad-qr".format(fn_path))
     scad_cmd = "openscad -o stls/{}-{}.stl qr2stls.scad -D is_qr={}".format(fn_base,"{}","{}")
     
     # first part
@@ -33,6 +33,8 @@ def process_url(url):
     os.system(cur_cmd)
     
 if __name__=="__main__":
+
+    FN = "urls/urls.txt"
     
-    for url in load_urls():
+    for url in load_urls(FN):
         process_url(url)
