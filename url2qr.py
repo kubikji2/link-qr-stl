d1ac7bb4c066c781b8168f0a15dc34983b614004
Jiri Kvant Kubik <jiri.kub@gmail.com>
2022-10-26 21:26:52 +0200

    [url-to-qr] refactoring and adding a single function to produce openscad array from url

--- url2qr.py
+++ url2qr.py
@@ -9,28 +9,28 @@ def print_arr(arr):
         print()
 
 # convert url to qr arr
-def url2arr(url):
-    # convert url to Pil Image 
-    qr_arr = qrcode.make(url, border=0, box_size=1)
+def url2qr(url):
+    # convert url to Pil Image representing QR code
+    qr_pil_img = qrcode.make(url, border=0, box_size=1)
     # get image size
-    x_size, y_size = qr_arr.size
-    img_arr = []
+    x_size, y_size = qr_pil_img.size
+    qr_arr = []
     for x in range(x_size):
         img_row = []
         for y in range(y_size):
-            px = qr_arr.getpixel((x,y))
+            px = qr_pil_img.getpixel((x,y))
             px //= 255
             img_row.append(px)
-        img_arr.append(img_row)
+        qr_arr.append(img_row)
 
-    return img_arr
+    return qr_arr
 
 # convert qr arr to openscad format string
-def arr2openscad(arr):
+def qr2openscad(qr_arr):
     openscad_arr = "[\n"
-    l = len(arr)
+    l = len(qr_arr)
     for row_i in range(l):
-        row = arr[row_i]
+        row = qr_arr[row_i]
         openscad_arr += "[ {} ]".format(", ".join([str(el) for el in row]))
         openscad_arr += ",\n" if row_i != l-1 else "\n]; \n"
     return openscad_arr
@@ -40,9 +40,15 @@ def save_to_file(arr, var_name, fn):
     with open(fn, 'w') as f:
         f.writelines("{} = {}".format(var_name,arr))
 
+# takes 'url' and save binary representation of QR code into 'file_name' in OpenSCAD array format as 'variable_name'
+def process_url_to_file(url, file_name, variable_name):
+    _arr = url2qr(url)
+    oscad_arr = qr2openscad(_arr)
+    save_to_file(oscad_arr, variable_name, file_name)
+
 # just a testing main
 if __name__=="__main__":
     URL = "https://store.steampowered.com/app/1942120/"
-    _arr = url2arr(URL)
-    oscad_arr = arr2openscad(_arr)
+    _arr = url2qr(URL)
+    oscad_arr = qr2openscad(_arr)
     save_to_file(oscad_arr, "test_arr", "./qrs/test.scad-qr")
